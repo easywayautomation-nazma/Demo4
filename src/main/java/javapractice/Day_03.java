ab1986c6d32926ee437e20bdd4f432fbc4fc8b48
easywayautomation-nazma <easywayautomationnazma@gmail.com>
2020-08-19 22:06:28 -0400

    Day_03

---
+++ src/main/java/javapractice/Day_03.java
@@ -0,0 +1,81 @@
+package javapractice;
+
+public class Day_03 {
+	
+	public static void main(String[] args) {
+		
+		//int num = 4;
+		
+		//if (num==2) {
+			//System.out.println(num);
+		//} else {
+			//System.out.println(num);
+		//}
+		
+		
+		
+		
+		
+			
+		//int num = 4;
+		
+		//if (num==2) {
+			//System.out.println(num);	
+		//} else if (num==3) {
+			//System.out.println(num);
+		//} else if (num==4) {
+			//System.out.println(num);
+		//} else {
+			//System.out.println(num);
+		//}
+		
+		
+		
+		
+		//int x=30;
+		//if (x==10) {
+			//System.out.println("Value of x is 10");
+			
+		//} else if (x==30) {
+			//System.out.println("Value of x is 20");
+		//} else if (x==30) {
+			//System.out.println("Value of x is 30");
+		//} else {
+			//System.out.println("This is else statement");
+		//}
+			
+			
+	//String name = "Nafiz";
+	
+	//if (name.equals("Hasan")) {
+		//System.out.println("Hasan");
+		
+	//}else if (name.equals("Sajib")) {
+		//System.out.println("Sajib");
+		
+	//}else if (name.equals("Nafiz")) {
+		//System.out.println("Nafiz");
+		
+	//} else {
+		//System.out.println("Nazma");
+	//}
+		
+		
+	
+		int num=1234;
+		if (num<100 && num>1) {
+			System.out.println("Its a two digit number");
+		}else if (num<1000 && num>=100) {
+			System.out.println("Its a two digit number");
+		}else if (num<10000 && num>=1000) {
+			System.out.println("Its a two digit number");
+		}else if (num<100000 && num>10000) {
+			System.out.println("Its a two digit number");
+		} else {
+			System.out.println("number is not between 1 & 99999");
+		}
+		
+			
+	}
+
+}
